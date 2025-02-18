14303e8a8d6441a1fab7262006c94f21a294ea9f
TsaiGai <iantsai11267@gmail.com>
2025-02-17 17:37:44 -0800

    Fix AuthContext

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import { auth } from "../../firebase/firebase"; // Firebase authentication instance
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 
 const AuthContext = createContext();
